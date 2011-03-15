822cb2a6e65a071adf4d0a1626bdb6b176fa010a
Boris Kolpackov <boris@codesynthesis.com>
2011-03-15 12:07:40 +0200

    Bump version to 1.2.0

--- odb/boost/version.hxx
+++ odb/boost/version.hxx
@@ -29,15 +29,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 10100
+#if ODB_VERSION != 10200
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-boost version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_BOOST_VERSION     1010000
-#define LIBODB_BOOST_VERSION_STR "1.1.0"
+#define LIBODB_BOOST_VERSION     1020000
+#define LIBODB_BOOST_VERSION_STR "1.2.0"
 
 #include <odb/post.hxx>
 
