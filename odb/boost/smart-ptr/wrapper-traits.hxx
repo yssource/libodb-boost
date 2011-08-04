a815b90fe870d6d740c91a60ec0633f8bc256337
Boris Kolpackov <boris@codesynthesis.com>
2011-08-04 13:32:25 +0200

    Add support for boost::optional and boost::shared_ptr as value wrappers
    New test: boost/common/optional.

---
+++ odb/boost/smart-ptr/wrapper-traits.hxx
@@ -0,0 +1,60 @@
+// file      : odb/boost/smart-ptr/wrapper-traits.hxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef ODB_BOOST_SMART_PTR_WRAPPER_TRAITS_HXX
+#define ODB_BOOST_SMART_PTR_WRAPPER_TRAITS_HXX
+
+#include <odb/pre.hxx>
+
+#include <boost/shared_ptr.hpp>
+
+#include <odb/wrapper-traits.hxx>
+
+namespace odb
+{
+  // Specialization for boost::shared_ptr.
+  //
+  template <typename T>
+  class wrapper_traits< ::boost::shared_ptr<T> >
+  {
+  public:
+    typedef T wrapped_type;
+    typedef ::boost::shared_ptr<T> wrapper_type;
+
+    static const bool null_handler = true;
+    static const bool null_default = false;
+
+    static bool
+    get_null (const wrapper_type& p)
+    {
+      return !p;
+    }
+
+    static void
+    set_null (wrapper_type& p)
+    {
+      p.reset ();
+    }
+
+    static const wrapped_type&
+    get_ref (const wrapper_type& p)
+    {
+      return *p;
+    }
+
+    static wrapped_type&
+    set_ref (wrapper_type& p)
+    {
+      if (!p)
+        p.reset (new wrapped_type);
+
+      return *p;
+    }
+  };
+}
+
+#include <odb/post.hxx>
+
+#endif // ODB_BOOST_SMART_PTR_WRAPPER_TRAITS_HXX
