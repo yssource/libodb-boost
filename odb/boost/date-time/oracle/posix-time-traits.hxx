a7e2b5e983ce12317bbc26d041a9a2a082840726
Constantin Michael <constantin@codesynthesis.com>
2011-11-07 15:21:17 +0200

    Implement Boost profile for Oracle

---
+++ odb/boost/date-time/oracle/posix-time-traits.hxx
@@ -0,0 +1,174 @@
+// file      : odb/boost/date-time/oracle/posix-time-traits.hxx
+// author    : Constantin Michael <constantin@codesynthesis.com>
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#ifndef ODB_BOOST_DATE_TIME_ORACLE_POSIX_TIME_TRAITS_HXX
+#define ODB_BOOST_DATE_TIME_ORACLE_POSIX_TIME_TRAITS_HXX
+
+#include <odb/pre.hxx>
+
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+
+#include <odb/core.hxx>
+
+#include <odb/oracle/traits.hxx>
+#include <odb/oracle/oracle-fwd.hxx>   // ub1, sb2, ub4
+#include <odb/oracle/oracle-types.hxx> // odb::oracle::datetime,
+                                       // odb::oracle::interval_ds
+
+#include <odb/boost/date-time/exceptions.hxx>
+
+namespace odb
+{
+  namespace oracle
+  {
+    template <>
+    struct default_value_traits< ::boost::posix_time::ptime, id_timestamp>
+    {
+      typedef ::boost::posix_time::ptime ptime;
+      typedef ::boost::posix_time::time_duration time_duration;
+      typedef ::boost::gregorian::date date;
+
+      typedef ptime value_type;
+      typedef ptime query_type;
+      typedef datetime image_type;
+
+      static void
+      set_value (ptime& v, const datetime& i, bool is_null)
+      {
+        if (is_null)
+          v = ptime (::boost::date_time::not_a_date_time);
+        else
+        {
+          sb2 y (0);
+          ub1 m (0), d (0), h (0), min (0), s (0);
+          ub4 ns;
+
+          i.get (y, m, d, h, min, s, ns);
+
+          unsigned long long fract_s (ns);
+          fract_s = fract_s * time_duration::ticks_per_second () /
+            1000000000ULL;
+
+          v = ptime (
+            date (static_cast<date::year_type> (y),
+                  static_cast<date::month_type> (m),
+                  static_cast<date::day_type> (d)),
+            time_duration (
+              static_cast<time_duration::hour_type> (h),
+              static_cast<time_duration::min_type> (min),
+              static_cast<time_duration::sec_type> (s),
+              static_cast<time_duration::fractional_seconds_type> (fract_s)));
+        }
+      }
+
+      static void
+      set_image (datetime& i, bool& is_null, const ptime& v)
+      {
+        if (v.is_special ())
+        {
+          if (v.is_not_a_date_time ())
+            is_null = true;
+          else
+            throw odb::boost::date_time::special_value ();
+        }
+        else
+        {
+          is_null = false;
+
+          const date& d (v.date ());
+          const time_duration& t (v.time_of_day ());
+
+          unsigned long long ns (t.fractional_seconds ());
+          ns = ns * 1000000000ULL / time_duration::ticks_per_second ();
+
+          i.set (static_cast<sb2> (d.year ()),
+                 static_cast<ub1> (d.month ()),
+                 static_cast<ub1> (d.day ()),
+                 static_cast<ub1> (t.hours ()),
+                 static_cast<ub1> (t.minutes ()),
+                 static_cast<ub1> (t.seconds ()),
+                 static_cast<ub4> (ns));
+        }
+      }
+    };
+
+    template <>
+    struct default_value_traits< ::boost::posix_time::time_duration,
+                                 id_interval_ds>
+    {
+      typedef ::boost::posix_time::time_duration time_duration;
+
+      typedef time_duration value_type;
+      typedef time_duration query_type;
+      typedef interval_ds image_type;
+
+      static void
+      set_value (time_duration& v,
+                 const interval_ds& i,
+                 bool is_null)
+      {
+        if (is_null)
+          v = time_duration (::boost::date_time::not_a_date_time);
+        else
+        {
+          sb4 d (0), h (0), m (0), s (0), ns (0);
+          i.get (d, h, m, s, ns);
+
+          unsigned long long fract_s (ns);
+          fract_s = fract_s * time_duration::ticks_per_second () /
+            1000000000ULL;
+
+          v = time_duration (
+            static_cast<time_duration::hour_type> (
+              static_cast<unsigned long long> (d) * 24 + h),
+            static_cast<time_duration::min_type> (m),
+            static_cast<time_duration::sec_type> (s),
+            static_cast<time_duration::fractional_seconds_type> (fract_s));
+        }
+      }
+
+      static void
+      set_image (interval_ds& i,
+                 bool& is_null,
+                 const time_duration& v)
+      {
+        if (v.is_special ())
+        {
+          if (v.is_not_a_date_time ())
+            is_null = true;
+          else
+            throw odb::boost::date_time::special_value ();
+        }
+        else
+        {
+          is_null = false;
+
+          unsigned long long ns (v.fractional_seconds ());
+          ns = ns * 1000000000ULL / time_duration::ticks_per_second ();
+
+          i.set (0,
+                 static_cast<sb4> (v.hours ()),
+                 static_cast<sb4> (v.minutes ()),
+                 static_cast<sb4> (v.seconds ()),
+                 static_cast<sb4> (ns));
+        }
+      }
+    };
+
+    template <>
+    struct default_type_traits< ::boost::posix_time::ptime>
+    {
+      static const database_type_id db_type_id = id_timestamp;
+    };
+
+    template <>
+    struct default_type_traits< ::boost::posix_time::time_duration>
+    {
+      static const database_type_id db_type_id = id_interval_ds;
+    };
+  }
+}
+
+#endif // ODB_BOOST_DATE_TIME_ORACLE_POSIX_TIME_TRAITS_HXX
