8839a4a2200a2f726830ca3f1e689eefb120191f
Boris Kolpackov <boris@codesynthesis.com>
2012-01-29 16:37:05 +0200

    Bump version to 1.8.0

--- odb/boost/version.hxx
+++ odb/boost/version.hxx
@@ -28,23 +28,23 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 10700
+#if ODB_VERSION != 10800
 #  error incompatible odb interface version detected
 #endif
 
 // ODB Boost interface version: odb interface version plus the Boost interface
 // version.
 //
-#define ODB_BOOST_VERSION     1070000
-#define ODB_BOOST_VERSION_STR "1.7.0"
+#define ODB_BOOST_VERSION     1080000
+#define ODB_BOOST_VERSION_STR "1.8.0"
 
 // libodb-boost version: odb interface version plus the bugfix version. Note
 // that LIBODB_BOOST_VERSION is always greater or equal to ODB_BOOST_VERSION
 // since if the Boost interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_BOOST_VERSION     1070000
-#define LIBODB_BOOST_VERSION_STR "1.7.0"
+#define LIBODB_BOOST_VERSION     1080000
+#define LIBODB_BOOST_VERSION_STR "1.8.0"
 
 #include <odb/post.hxx>
 
