47fa3e70fe257e979520d1c7f8470b10aef01dc9
Boris Kolpackov <boris@codesynthesis.com>
2013-06-21 12:47:46 +0200

    Bump version to 2.2.1

--- odb/boost/version.hxx
+++ odb/boost/version.hxx
@@ -43,8 +43,8 @@
 // since if the Boost interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_BOOST_VERSION     2020000
-#define LIBODB_BOOST_VERSION_STR "2.2.0"
+#define LIBODB_BOOST_VERSION     2020100
+#define LIBODB_BOOST_VERSION_STR "2.2.1"
 
 #include <odb/post.hxx>
 
