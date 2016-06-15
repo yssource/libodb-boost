004beb430ede053d256308fe9516970ccbc4c6f8
Boris Kolpackov <boris@codesynthesis.com>
2016-06-15 18:56:49 +0200

    Bump version to 2.5.0.a8

--- odb/boost/version.hxx
+++ odb/boost/version.hxx
@@ -28,23 +28,23 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20407
+#if ODB_VERSION != 20408
 #  error incompatible odb interface version detected
 #endif
 
 // ODB Boost interface version: odb interface version plus the Boost interface
 // version.
 //
-#define ODB_BOOST_VERSION     2040700
-#define ODB_BOOST_VERSION_STR "2.5.0.a7"
+#define ODB_BOOST_VERSION     2040800
+#define ODB_BOOST_VERSION_STR "2.5.0.a8"
 
 // libodb-boost version: odb interface version plus the bugfix version. Note
 // that LIBODB_BOOST_VERSION is always greater or equal to ODB_BOOST_VERSION
 // since if the Boost interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_BOOST_VERSION     2049907
-#define LIBODB_BOOST_VERSION_STR "2.5.0.a7"
+#define LIBODB_BOOST_VERSION     2049908
+#define LIBODB_BOOST_VERSION_STR "2.5.0.a8"
 
 #include <odb/post.hxx>
 
